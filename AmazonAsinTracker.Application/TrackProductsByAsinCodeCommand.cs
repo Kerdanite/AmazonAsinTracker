e37ee0708e3c36f55a2a257f4f2d0ccb5f2618a3
Joffrey Truyen <jtruyen@cirilgroup.com>
2021-06-22 12:04:44 +0200

    WebApi squeletton and application command handler with mediatr empty implemantion

---
+++ AmazonAsinTracker.Application/TrackProductsByAsinCodeCommand.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace AmazonAsinTracker.Application
+{
+    public class TrackProductsByAsinCodeCommand : IRequest
+    {
+        public IEnumerable<string> ProductAsins { get; set; }
+    }
+
+    public class TrackProductsByAsinCodeCommandHandler : IRequestHandler<TrackProductsByAsinCodeCommand>
+    {
+        public async Task<Unit> Handle(TrackProductsByAsinCodeCommand request, CancellationToken cancellationToken)
+        {
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ AmazonAsinTrackerApi/Controllers/AmazonAsinTrackerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using AmazonAsinTracker.Application;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AmazonAsinTrackerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AmazonAsinTrackerController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AmazonAsinTrackerController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        [HttpPut]
+        [ProducesResponseType((int)HttpStatusCode.Accepted)]
+        public async Task<ActionResult> TrackAsin(string asinCode)
+        {
+            await _mediator.Send(new TrackProductsByAsinCodeCommand
+            {
+                ProductAsins = new List<string>(){asinCode}
+            });
+
+            return Accepted();
+        }
+
+    }
+}
